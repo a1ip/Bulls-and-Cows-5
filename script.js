0ec3351a78fae49ef97cb52a7dda9f6f5c0013a5
mkukolev <michael.kukolev.phone@gmail.com>
2018-05-15 16:40:18 +0300

    change js file
    add main function

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 
-function calc() {
-    var randomNum = Math.ceil((Math.random() * 9));
-    return randomNum;
-}
+// function calc() {
+//     var randomNum = Math.ceil((Math.random() * 9));
+//     return randomNum;
+// }
 
-var resultNum = calc.apply();
-console.log(resultNum);
+// var resultNum = calc.apply();
+// console.log(resultNum);
+
+// function main() {
+//     var arr = [];
+//     while (arr.length < 4) {
+//         var randomNum = Math.ceil((Math.random() * 9));
+//         var found = false;
+//     for (var i = 0; i < 10; i++) {
+//         if (arr[i] === randomNum) {
+//             var found = true;
+//             break;
+//             }
+//         }
+//         if (!found) arr[arr.length] = randomNum;
+//     }
+// } 
+
+
+function main(secretNum, guess) {
+   var bulls = 0;
+   var cows = 0;
+   var randomNum = Math.ceil((Math.random() * 9));
+   var numbers = new Array(10);
+    for (i = 0; i < 10; i++) {
+        numbers[i] = 0;
+        }
+    // for (i = 0; i < secretNum; i++) {
+    //     var secret 
+    //     var guess
+    // }
+    
+}
 
